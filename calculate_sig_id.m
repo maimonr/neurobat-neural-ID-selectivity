b8613c8d1ca6d01bef7a3fc89228abece8d93980
Maimon rose <maimon.rose@gmail.com>
2020-10-23 15:16:39 -0700

    moved calculate_sig_id function to this repo

---
+++ calculate_sig_id.m
@@ -0,0 +1,44 @@
+function [sigIdx,callSigIdx] = calculate_sig_id(bat_id_pred_all,varIdx,varargin)
+
+pnames = {'callAccFlag','correctionType','alpha0'};
+dflts  = {false,'BH',0.05};
+[callAccFlag,correctionType,alpha0] = internal.stats.parseArgs(pnames,dflts,varargin{:});
+
+all_cells = unique(bat_id_pred_all(:,varIdx), 'rows');
+[sigIdx,callSigIdx] = deal(false(1,size(bat_id_pred_all,1)));
+for k = 1:size(all_cells,1)
+    idx = find(ismember(bat_id_pred_all(:,varIdx),all_cells(k,:)));
+    p = 1 - cellfun(@(acc,bootAcc) sum(acc > bootAcc)/length(bootAcc),num2cell(bat_id_pred_all.acc(idx)),bat_id_pred_all.bootAcc(idx,:));
+    sigIdx(idx) = calculate_sig(p,alpha0,correctionType);
+    
+    if callAccFlag
+        usedIdx = cellfun(@(bootAcc) ~all(bootAcc==0),bat_id_pred_all.bootCallAcc(idx,:));
+        idx = idx(usedIdx);
+        if ~isempty(idx)
+            p = 1 - cellfun(@(acc,bootAcc) sum(acc > bootAcc)/length(bootAcc),num2cell(bat_id_pred_all.callAcc(idx)),bat_id_pred_all.bootCallAcc(idx,:));
+            callSigIdx(idx) = calculate_sig(p,alpha0,correctionType);
+        end
+    end
+end
+end
+
+function sigIdx = calculate_sig(p,alpha0,correctionType)
+
+m = length(p);
+switch correctionType
+    case 'none'
+        sigIdx = p < alpha0;
+    case 'BH'
+        [pSort,p_sort_idx] = sort(p,'ascend');
+        BH_critical_value = alpha0*((1:m)/m)';
+        max_sig_idx = find(pSort < BH_critical_value, 1, 'last' );
+        
+        sigIdx = false(1,m);
+        
+        current_sig_idx = p_sort_idx(1:max_sig_idx);
+        sigIdx(current_sig_idx) = true;
+    case 'bonferroni'
+        sigIdx = p < alpha0/m;
+end
+
+end
